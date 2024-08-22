6f9ac25ef1295104a235b9bc237ff3f03541374a
manojrella <116781379+manojrella@users.noreply.github.com>
2024-08-22 22:11:35 +0530

    commiyt

--- src/main/java/com/example/CheckGitRepo/CheckGitRepoApplication.java
+++ src/main/java/com/example/CheckGitRepo/CheckGitRepoApplication.java
@@ -8,6 +8,8 @@ public class CheckGitRepoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CheckGitRepoApplication.class, args);
+		
+		
 		//Just Comment I wanted To Add
 	}
 
